553df2004bcbd1af64a3d31e11fd433941a74586
toma satou <84651853+sato-toma@users.noreply.github.com>
2023-08-18 02:10:38 +0900

    remove all

--- gs-marker/src/components/atoms/AppLogo/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Meta } from '@storybook/react';
-import AppLogo from './index';
-
-export default { title: 'Atoms/AppLogo' } as Meta<typeof AppLogo>;
-
-export const Logo = () => <AppLogo />;
--- gs-marker/src/components/organisms/Footer/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Meta } from '@storybook/react';
-import Footer from './';
-
-export default { title: 'Organisms/Footer' } as Meta<typeof Footer>;
-
-export const Standard = () => <Footer />;
